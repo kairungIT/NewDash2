e35c32317e5ad5f4935ade1ee3accd73094d4b0f
kairung <kairung@webmail.npru.ac.th>
2023-10-05 04:23:50 +0700

    .commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # ใช้ HTML สำหรับการเชื่อมโยงไฟล์ CSS
 html_string = "<h3>this is an html string</h3>"
 st.markdown(html_string, unsafe_allow_html=True)
-
+st.markdown("<link rel='stylesheet' href='styles.css'>", unsafe_allow_html=True)
 st.write("<p style='color: red;'>This text is red.</p>")
 
 # สร้างข้อมูลหลักในแอพพลิเคชัน
