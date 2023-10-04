9b7bcabd30e69f150d65985549284a4381cba793
kairung <kairung@webmail.npru.ac.th>
2023-10-05 04:22:17 +0700

    .commit

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
 # ใช้ HTML สำหรับการเชื่อมโยงไฟล์ CSS
-st.markdown("""
-<link rel="stylesheet" href="style.css" button class="button">Green</link>
-<link rel="stylesheet" href="style.css" button class="button button2">Blue</link>
-<link rel="stylesheet" href="style.css" button class="button button3">Red</link>
-<link rel="stylesheet" href="style.css" button class="button button4">Gray</link>
-<link rel="stylesheet" href="style.css" button class="button button5">Black</link> 
-""", unsafe_allow_html=True)
+html_string = "<h3>this is an html string</h3>"
+st.markdown(html_string, unsafe_allow_html=True)
+
+st.write("<p style='color: red;'>This text is red.</p>")
 
 # สร้างข้อมูลหลักในแอพพลิเคชัน
 st.title('แอพพลิเคชัน Streamlit ที่ได้รับการตกแต่ง')
