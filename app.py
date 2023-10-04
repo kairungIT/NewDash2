476a1815749fd8b2b89c7218e0d80f2500f04bb4
kairung <kairung@webmail.npru.ac.th>
2023-10-05 04:04:43 +0700

    .commit

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 import streamlit as st
 
-# สร้างแหล่งข้อมูลเพื่อเก็บข้อมูลปุ่ม
-button_colors = {
-    'Red': 'red',
-    'Green': 'green',
-    'Blue': 'blue'
-}
+# ใช้ HTML สำหรับการเชื่อมโยงไฟล์ CSS
+st.markdown('<link rel="stylesheet" href="style.css">', unsafe_allow_html=True)
 
-# สร้างแบบฟอร์มสำหรับเลือกสี
-selected_color = st.selectbox('เลือกสี:', list(button_colors.keys()))
-
-# สร้างปุ่มที่มีสีตามที่ผู้ใช้เลือก
-if st.button('แสดงผล'):
-    color = button_colors[selected_color]
-    st.write(f'คุณเลือกสี: {selected_color}')
-    st.write(f'สีที่คุณเลือกคือ {color}', unsafe_allow_html=True)
-
-# แสดงสไลด์เดอร์เพื่อเปลี่ยนค่าอื่น ๆ
-st.sidebar.header('ตัวเลือก')
-text_input = st.sidebar.text_input('ใส่ข้อความ:')
-number_input = st.sidebar.number_input('ใส่ตัวเลข:', min_value=0)
-
-# แสดงผลข้อความและตัวเลข
-st.sidebar.write(f'ข้อความที่คุณใส่: {text_input}')
-st.sidebar.write(f'ตัวเลขที่คุณใส่: {number_input}')
+# สร้างข้อมูลหลักในแอพพลิเคชัน
+st.title('แอพพลิเคชัน Streamlit ที่ได้รับการตกแต่ง')
