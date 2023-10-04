6fe6b86ca15c66d6d82e02b12ebafb4c2ac0069c
kairung <kairung@webmail.npru.ac.th>
2023-10-05 04:15:30 +0700

    .commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # ใช้ HTML สำหรับการเชื่อมโยงไฟล์ CSS
-st.markdown("""<link rel="stylesheet" href="style.css">
-<button class="button">Green</button>
-<button class="button button2">Blue</button>
-<button class="button button3">Red</button>
-<button class="button button4">Gray</button>
-<button class="button button5">Black</button> </link>
+st.markdown("""
+<link rel="stylesheet" href="style.css" button class="button">Green</link>
+<link rel="stylesheet" href="style.css" button class="button button2">Blue</link>
+<link rel="stylesheet" href="style.css" button class="button button3">Red</link>
+<link rel="stylesheet" href="style.css" button class="button button4">Gray</link>
+<link rel="stylesheet" href="style.css" button class="button button5">Black</link> 
 """, unsafe_allow_html=True)
 
 # สร้างข้อมูลหลักในแอพพลิเคชัน
